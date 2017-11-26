83470af58c934c87e7b44612a6d2697cd14c4a84
Thomas Mettam <tom@caspertech.co.uk>
2017-11-26 03:10:54 +0000

    InstantMessage helper function, fix issue with failing to use the returned buffer

--- example/testBot.js
+++ example/testBot.js
@@ -10,13 +10,16 @@ loginParameters.start = "last";
 
 const bot = new nmv.Bot(loginParameters);
 
+let resp = null;
+
 bot.Login().then((response) =>
 {
     //Establish circuit wit region
+    resp = response;
     return response.region.circuit.establish(response.agent.agentID);
 }).then(() =>
 {
-    console.log("Region circuit established");
+    resp.region.circuit.sendInstantMessage(resp.agent.agentID, "dbcd7dfe-a5db-4736-91bc-2af1e69902e6", "FUCK YOU");
 }).catch((error) =>
 {
     console.log("Error:");
