ac6585a3ae63c7f61bd6b54e1f96365f152b700d
Thomas Mettam <tom@caspertech.co.uk>
2017-12-14 02:24:06 +0000

    When StoreMyAttachmentsOnly is set, Drop all other objects as soon as we know our agent localID

--- lib/classes/ObjectStoreLite.ts
+++ lib/classes/ObjectStoreLite.ts
@@ -86,6 +86,18 @@ export class ObjectStoreLite implements IObjectStore
                         if (objData.PCode === PCode.Avatar && this.objects[localID].FullID.toString() === this.agent.agentID.toString())
                         {
                             this.agent.localID = localID;
+
+                            if (this.options & BotOptionFlags.StoreMyAttachmentsOnly)
+                            {
+                                Object.keys(this.objectsByParent).forEach((objParentID: string) =>
+                                {
+                                    const parent = parseInt(objParentID, 10);
+                                    if (parent !== this.agent.localID)
+                                    {
+                                        this.deleteObject(parent);
+                                    }
+                                });
+                            }
                         }
 
                         this.objectsByUUID[objData.FullID.toString()] = localID;
