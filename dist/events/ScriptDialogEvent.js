c7be7a380c13f9934cd7c49b5b643fcd1cb70e4f
hintswen <hintswen@users.noreply.github.com>
2018-10-07 22:26:39 +1100

    Tried to add a ScriptDialog event...
    It's currently not working, not sure what I missed.

---
+++ dist/events/ScriptDialogEvent.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class ScriptDialogEvent {
+}
+exports.ScriptDialogEvent = ScriptDialogEvent;
+//# sourceMappingURL=ScriptDialogEvent.js.map
