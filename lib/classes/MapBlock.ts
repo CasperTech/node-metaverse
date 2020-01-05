4eb905ed7f5d721bdf5ffd70c35a2802b3278a5e
Tom Mettam <tom@caspertech.co.uk>
2020-01-05 00:30:13 +0000

    Supply more information with a MapBlock

--- lib/classes/MapBlock.ts
+++ lib/classes/MapBlock.ts
@@ -1,8 +1,13 @@
 import {UUID} from './UUID';
+import { RegionFlags } from '..';
 
 export class MapBlock
 {
     name: string;
     mapImage: UUID;
     accessFlags: number;
+    x: number;
+    y: number;
+    waterHeight: number;
+    regionFlags: RegionFlags;
 }
